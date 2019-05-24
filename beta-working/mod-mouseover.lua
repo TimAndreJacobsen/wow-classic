bc14a7b1eb9f9f0a66b0a902fde5344da58f9ea6
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-05-24 20:21:05 +0200

    Create mod-mouseover.lua

---
+++ beta-working/mod-mouseover.lua
@@ -0,0 +1,13 @@
+--- normal keypress uses ability on target, modified keypress(shift/alt/ctrl) gets target from mouseover ---
+--- usage: /cast [mod <modifier-key> target] ability
+--- usage: /cast [mod none @target] ability
+
+--- example:
+/cast [mod shift @mouseover] Kick
+/cast [mod none @target] Kick
+
+/cast [mod shift @mouseover] Sap
+/cast [mod none @target] Sap
+
+/cast [mod shift @mouseover] Blind
+/cast [mod none @target] Blind
