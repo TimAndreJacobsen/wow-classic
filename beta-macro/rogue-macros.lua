6ad2db9a0ce6da735b7555eacd305699aa425ad1
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-06-04 18:04:35 +0200

    pickpocket on rogue opener macro's

---
+++ beta-macro/rogue-macros.lua
@@ -0,0 +1,19 @@
+--- Rogue auto-pickpocket on opener ---
+--- Be sure to enable auto-looting in options or this will not work
+
+#showtooltip Garrote
+/cast Pickpocket
+/cast Garrote
+
+#showtooltip Cheap Shot
+/cast Pickpocket
+/cast Cheap Shot
+
+#showtooltip Ambush
+/cast Pickpocket
+/cast Ambush
+
+--- If pickpocket resists you might not be able to sap!
+#showtooltip Sap
+/cast Pickpocket
+/cast Sap
